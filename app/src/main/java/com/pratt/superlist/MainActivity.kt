501960c9e885c622ed1af39390c09b3957881e14
PetePrattis <panagiotispr1997@gmail.com>
2020-10-12 16:01:09 +0300

    update db child "name"

--- app/src/main/java/com/pratt/superlist/MainActivity.kt
+++ app/src/main/java/com/pratt/superlist/MainActivity.kt
@@ -2,10 +2,37 @@ package com.pratt.superlist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_main.*
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
+
 
 class MainActivity : AppCompatActivity() {
+
+    // [START declare_database_ref]
+    private lateinit var database: DatabaseReference
+    // [END declare_database_ref]
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // [START initialize_database_ref]
+        database = Firebase.database.reference
+        // [END initialize_database_ref]
+
+        Update.setOnClickListener(){ updateDB() }
+
+    }
+
+    fun updateDB(){
+        val name = SuperName.text .toString()
+        database.child("list").child("items").child("name").setValue(name)
+        SuperName.setText("")
     }
 }
