b7f605f68b5f6f286571bdc55172190cc3099c11
PetePrattis <panagiotispr1997@gmail.com>
2020-10-21 18:21:50 +0300

    main activity new household dialog inflate view

--- app/src/main/java/com/pratt/superlist/MainActivity.kt
+++ app/src/main/java/com/pratt/superlist/MainActivity.kt
@@ -4,6 +4,10 @@ import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Button
+import android.widget.LinearLayout
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.facebook.AccessToken
 import com.facebook.FacebookSdk
@@ -15,10 +19,13 @@ import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
+import kotlinx.android.synthetic.main.layout_dialognewhousehold.view.*
+import java.lang.Exception
+import java.lang.NullPointerException
 
 
 class MainActivity : AppCompatActivity() {
-
     private val TAG = MainActivity::class.qualifiedName
     // [START declare_database_ref]
     private lateinit var database: DatabaseReference
@@ -36,14 +43,18 @@ class MainActivity : AppCompatActivity() {
         val uemail = FirebaseAuth.getInstance().currentUser?.email
         val username = FirebaseAuth.getInstance().currentUser?.displayName
 
+        newhouseholdb.setOnClickListener {
+            openDialog()
+        }
 
-        uname.text = username
+
 
         signoutb.setOnClickListener {
             singoutUser()
         }
 
     }
+
     private fun singoutUser(){
         FirebaseAuth.getInstance().signOut()
         LoginManager.getInstance().logOut();
@@ -54,5 +65,37 @@ class MainActivity : AppCompatActivity() {
     }
 
 
+    fun openDialog(){
+        try {
+            //inflate dialog with custom view
+            val dialogNewHouseholdView = LayoutInflater.from(this).inflate(R.layout.layout_dialognewhousehold,null, false)
+            //AlertDialogBuilder
+            val builder = AlertDialog.Builder(this)
+                .setView(dialogNewHouseholdView)
+                .setTitle("Create New Household")
+            //show dialog
+            val mAlertDialog = builder.show()
+
+            //create button click custom layout
+            dialogNewHouseholdView.newhouseholdb.setOnClickListener {
+                //check if inputs exist
+                //dismiss dialog
+                mAlertDialog.dismiss()
+                //get household info
+                val householdname = dialogNewHouseholdView.hnameet.text.toString()
+                val householdpassword = dialogNewHouseholdView.hpasswordet.text.toString()
+                //create household child in firebase realtime db
+
+            }
+            dialogNewHouseholdView.dialogcancelhouseholdb.setOnClickListener {
+                //dismiss dialog
+                mAlertDialog.dismiss()
+            }
+        }
+        catch (e: Exception){
+
+        }
+
+    }
 
 }
